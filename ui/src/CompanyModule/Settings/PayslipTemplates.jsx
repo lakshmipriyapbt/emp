5befa0f9dfe913ea6a97d9c87bd1390150be6d1b
sandhya <sandhyak@pathbreakertech.in>
2025-02-04 17:14:37 +0530

    added status in employee module

--- ui/src/CompanyModule/Settings/PayslipTemplates.jsx
+++ ui/src/CompanyModule/Settings/PayslipTemplates.jsx
@@ -20,6 +20,9 @@ const PayslipTemplates = () => {
         }
       }, [logoFileName]); 
 
+    console.log("companyId:",user.companyId);
+    
+
     const templates = [
         { id: 1, title: 'Template 1', bgColor: '#e6e6fa', headingColor: '#4682b4', component: <PayslipTemplate1 /> },
         { id: 2, title: 'Template 2', bgColor: '#e6e6fa', headingColor: '#4682b4', component: <PayslipTemplate2 /> },
