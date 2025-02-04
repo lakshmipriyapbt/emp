73f78026a5b6c8dfeb521b193e4aadca41b55da5
narendrabpbt2023 <narendrab@pathbreakertech.in>
2025-02-04 15:26:17 +0530

    changed the issues

--- ui/src/CompanyModule/Settings/Experience/ExperienceLetter.js
+++ ui/src/CompanyModule/Settings/Experience/ExperienceLetter.js
@@ -9,48 +9,49 @@ import ExperienceTemplate2 from "./ExperienceTemplate2";
 const ExperienceLetter = () => {
   const [selectedTemplate, setSelectedTemplate] = useState(null);
   const [activeCardIndex, setActiveCardIndex] = useState(null);
+    const [companyData, setCompanyData] = useState({});
   const [fetchedTemplate, setFetchedTemplate] = useState(null);
   const [employeeDetails, setEmployeeDetails] = useState(null);
   const [loading, setLoading] = useState(false);
   const [isFetched, setIsFetched] = useState(false);
 
-  const {user,id, companyData,logoFileName} = useAuth();
+  const {user,id,logoFileName} = useAuth();
 
-  // const fetchCompanyData = async (companyId) => {
-  //   try {
-  //     const response = await companyViewByIdApi(companyId);
-  //     setCompanyData(response.data);
-  //   } catch (err) {
-  //     console.error("Error fetching company data:", err);
-  //     toast.error("Failed to fetch company data");
-  //   }
-  // };
-
-  // const fetchEmployeeDetails = async (employeeId) => {
-  //   try {
-  //     const response = await EmployeeGetApiById(employeeId);
-  //     setEmployeeDetails(response.data);
-  //     if (response.data.companyId) {
-  //       fetchCompanyData(response.data.companyId);
-  //     }
-  //   } catch (err) {
-  //     console.error("Error fetching employee details:", err);
-  //     toast.error("Failed to fetch employee details");
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   const userId = user.userId;
-  //   setLoading(true);
-  //   if (userId) {
-  //     fetchEmployeeDetails(userId);
-  //   }
-  //   setLoading(false);
-  // }, [user.userId]);
-
-  const fetchTemplate = async () => {
+  const fetchCompanyData = async (companyId) => {
     try {
-      const res = await TemplateGetAPI(id);
+      const response = await companyViewByIdApi(companyId);
+      setCompanyData(response.data);
+    } catch (err) {
+      console.error("Error fetching company data:", err);
+      toast.error("Failed to fetch company data");
+    }
+  };
+
+  const fetchEmployeeDetails = async (employeeId) => {
+    try {
+      const response = await EmployeeGetApiById(employeeId);
+      setEmployeeDetails(response.data);
+      if (response.data.companyId) {
+        fetchCompanyData(response.data.companyId);
+      }
+    } catch (err) {
+      console.error("Error fetching employee details:", err);
+      toast.error("Failed to fetch employee details");
+    }
+  };
+
+  useEffect(() => {
+    const userId = user.userId;
+    setLoading(true);
+    if (userId) {
+      fetchEmployeeDetails(userId);
+    }
+    setLoading(false);
+  }, [user.userId]);
+
+  const fetchTemplate = async (companyId) => {
+    try {
+      const res = await TemplateGetAPI(companyId);
       const templateNo = res.data.data.experienceTemplateNo; // Get the experience template number
       setFetchedTemplate(res.data.data); // Store fetched data
       setIsFetched(true); // Mark template as fetched
@@ -67,9 +68,9 @@ const ExperienceLetter = () => {
 
   useEffect(() => {
     if (companyData) {
-      fetchTemplate(id);
+      fetchTemplate(companyData.id);
     }
-  }, [companyData,id]);
+  }, [companyData]);
 
   const templates = useMemo(() => [
     {
