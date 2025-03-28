4a67fb3b489cc22f8a389c170e2a784f91a424e7
narasimhapbt <narasimha.k@vbrsit.com>
2025-03-28 11:05:14 +0530

    Filte the emplyee status

--- employee/src/main/java/com/pb/employee/util/EmployeeUtils.java
+++ employee/src/main/java/com/pb/employee/util/EmployeeUtils.java
@@ -513,7 +513,9 @@ public class EmployeeUtils {
 
         return employees.stream()
                 .filter(employee -> !employeesWithAttendance.contains(employee.getId()) &&
-                        !employeesWithAttendance.contains(employee.getEmployeeId()))
+                        !employeesWithAttendance.contains(employee.getEmployeeId())&&
+                !Constants.ACTIVE.equalsIgnoreCase(employee.getStatus()) &&
+                !Constants.ON_BOARDING.equalsIgnoreCase(employee.getStatus()))
                 .collect(Collectors.toList());
     }
 }
