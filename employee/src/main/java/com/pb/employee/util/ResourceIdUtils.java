63e80b040408367406797e0e97ff28926d8424ce
Simham1 <kagulanarasimha0806@gmail.com>
2025-05-14 17:46:47 +0530

    Clear the Designation issue

--- employee/src/main/java/com/pb/employee/util/ResourceIdUtils.java
+++ employee/src/main/java/com/pb/employee/util/ResourceIdUtils.java
@@ -49,8 +49,8 @@ public class ResourceIdUtils {
         return generateGlobalResourceId(ResourceType.DEPARTMENT, id, timestamp);
     }
 
-    public static String generateDesignationResourceId(String id, String timestamp) {
-        return generateGlobalResourceId(ResourceType.DESIGNATION, id, timestamp);
+    public static String generateDesignationResourceId(String id) {
+        return generateGlobalResourceId(ResourceType.DESIGNATION, id);
     }
     public static String generateAttendanceId(String company, String employeeId,String year,String month) {
         return generateGlobalResourceId(ResourceType.ATTENDANCE,company,year,month,employeeId);
